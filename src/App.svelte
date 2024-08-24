75b35598b9303ff9f701154f394b985408911fe5
uniboxx <unibox@duck.com>
2024-08-24 13:00:44 +0200

    modify App

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,7 @@
+<script></script>
+
 <h1>Svelte5 template</h1>
 
 <style lang="scss">
   @use './styles/vars';
-
-  h1 {
-    background-color: vars.$color-primary;
-    color: vars.$color-secondary;
-    font-weight: 300;
-    text-align: center;
-    padding: 1rem;
-  }
 </style>
